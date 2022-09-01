a0edf651d32a6a94faefd61292b3c82714c657d1
aryan1403 <aaryan14032006@gmail.com>
2022-09-01 14:01:43 +0530

    app updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
 
 function App() {
   return (
     <>
     <NavBar/>
+    <Login/>
     </>
   );
 }
